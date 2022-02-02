72e1090212d228a3b74d9331e88102dc838ab8f5
medeiros13 <gabrielmedeiros13122000@gmail.com>
2022-02-02 13:06:18 -0300

    Feitas melhorias com o uso do async e await, para substituir as tasks encadeadas

--- ByteBank.View/MainWindow.xaml.cs
+++ ByteBank.View/MainWindow.xaml.cs
@@ -32,9 +32,8 @@ namespace ByteBank.View
             r_Servico = new ContaClienteService();
         }
 
-        private void BtnProcessar_Click(object sender, RoutedEventArgs e)
+        private async void BtnProcessar_Click(object sender, RoutedEventArgs e)
         {
-            var taskSchedulerUI = TaskScheduler.FromCurrentSynchronizationContext();
             BtnProcessar.IsEnabled = false;
 
             var contas = r_Repositorio.GetContaClientes();
@@ -43,49 +42,26 @@ namespace ByteBank.View
 
             var inicio = DateTime.Now;
 
+            var resultado = await ConsolidarContas(contas);
 
-            //Aqui, encadeamos várias tarefas para serem executadas em ordem, a primeira (do primeiro ContinueWith), é responsável por atualizar a view
-            //a segunda, é responsável por tornar o botão enabled de novo
-            //OBS: para criarmos tarefas que mexam na UI, é necessário utilizarmos o taskScheduler da linha de execução principal, por isso, declaramos ele na linha 37, e estamos passando ele como o segundo parâmetro dos nossos ContinueWith
-            ConsolidarContas(contas)
-                .ContinueWith(task =>
-                {
-                    var fim = DateTime.Now;
-                    var resultado = task.Result;
-                    AtualizarView(resultado, fim - inicio);
-                }, taskSchedulerUI)
-                .ContinueWith(task =>
-                {
-                    BtnProcessar.IsEnabled = true;
-                }, taskSchedulerUI);
+            var fim = DateTime.Now;
+            AtualizarView(resultado, fim - inicio);
+            BtnProcessar.IsEnabled = true;
         }
 
-        //Esse método está retornando uma tarefa que contém uma lista de string dentro dela
-        //OBS: o tipo definido dentro da task (no nosso caso a lista de string) pode ser recuperado através da propriedade ".result", como foi feito na linha 54
-        private Task<List<string>> ConsolidarContas(IEnumerable<ContaCliente> contas)
+        private async Task<string[]> ConsolidarContas(IEnumerable<ContaCliente> contas)
         {
-            var resultado = new List<string>();
             var tasks = contas.Select(conta =>
-            {
-                return Task.Factory.StartNew(() =>
-                {
-                    var contaResultado = r_Servico.ConsolidarMovimentacao(conta);
-                    resultado.Add(contaResultado);
-                });
-            });
+                Task.Factory.StartNew(() => r_Servico.ConsolidarMovimentacao(conta)));
 
-            //Aqui nós temos uma task que espera que as tasks sejam finalizadas, pra depois, retornar a lista de string
-            return Task.WhenAll(tasks).ContinueWith(task =>
-            {
-                return resultado;
-            });
 
+            return await Task.WhenAll(tasks);
         }
 
-        private void AtualizarView(List<String> result, TimeSpan elapsedTime)
+        private void AtualizarView(IEnumerable<String> result, TimeSpan elapsedTime)
         {
             var tempoDecorrido = $"{ elapsedTime.Seconds }.{ elapsedTime.Milliseconds} segundos!";
-            var mensagem = $"Processamento de {result.Count} clientes em {tempoDecorrido}";
+            var mensagem = $"Processamento de {result.Count()} clientes em {tempoDecorrido}";
 
             LstResultados.ItemsSource = result;
             TxtTempo.Text = mensagem;
